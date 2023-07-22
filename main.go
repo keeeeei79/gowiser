8073f67a3c60bb92494b988f3c04162b5e6a8e5c
keeeeei79 <keeeeei79@gmail.com>
2023-07-22 21:09:29 +0900

    add mode

--- main.go
+++ main.go
@@ -1,7 +1,48 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	cli "github.com/urfave/cli/v2"
+)
+
+var (
+	Version = "0.0.1"
+)
+
+const (
+	name  = "gowiser"
+	usage = ""
+)
 
 func main() {
-	fmt.Println("HELLO")
+	app := cli.NewApp()
+	app.Version = Version
+	app.Name = name
+	app.Usage = usage
+	app.Commands = []*cli.Command{
+		{
+			Name:    "index",
+			Aliases: []string{"i"},
+			Usage:   "build index",
+			Action: func(c *cli.Context) error {
+				fmt.Println("Starting to build index...")
+				return nil
+			},
+		},
+		{
+			Name:    "search",
+			Aliases: []string{"s"},
+			Usage:   "search",
+			Action: func(c *cli.Context) error {
+				fmt.Println("Starting to search...")
+				return nil
+			},
+		},
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		panic(err)
+	}
 }
